1e8ce1739e3a98a8ffacb376d35ec1a651447204
a.martuhov <alexmartuhov@gmail.com>
2021-07-17 18:19:25 +0300

    homework4

---
+++ src/main/java/com/example/springapp/controller/BusController.java
@@ -0,0 +1,33 @@
+package com.example.springapp.controller;
+
+
+import com.example.springapp.controller.dto.BusDto;
+import com.example.springapp.service.BusService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bus")
+public class BusController {
+
+    private BusService busService;
+    public BusController (BusService busService){
+        this.busService=busService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BusDto> creatBus (@RequestBody BusDto bus) {
+        return new ResponseEntity<>(busService.creatBus(bus), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BusDto> getBus(@PathVariable Integer id) {
+        return new ResponseEntity<>(busService.getBus(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<BusDto> deleteBus (@PathVariable Integer id) {
+        return new ResponseEntity<>(busService.deleteBus(id), HttpStatus.OK);
+    }
+}
